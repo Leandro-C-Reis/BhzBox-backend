6284e0a39380432bf804043be4e6f67aae68fab0
leandro-c-reis <leandroreis02@gmail.com>
2020-10-05 23:11:16 -0300

    database connections updated for support development in linux

--- knexfile.ts
+++ knexfile.ts
@@ -6,12 +6,13 @@ dotenv.config({
 });
 
 module.exports = {
-  client: 'mysql2',
+  client: 'pg',
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,15 +5,18 @@ dotenv.config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
 
+const port = parseInt(process.env.DB_PORT = "");
+
 const connection = knex({
-    client: 'mysql2',
-    connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
-    useNullAsDefault: true,
+  client: 'pg',
+  connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: port
+  },
+  useNullAsDefault: true,
 })
 
 export default connection;
